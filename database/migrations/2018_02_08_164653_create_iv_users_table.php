db28855f1b0c1abc253c1cd2dfe421bf95fcdd80
Andrew Gilbert Moschou <amoschou@fastmail.com.au>
2018-02-11 00:55:45 +1030

    Fixed mysql and pgsql differences

--- database/migrations/2018_02_08_164653_create_iv_users_table.php
+++ database/migrations/2018_02_08_164653_create_iv_users_table.php
@@ -6,13 +6,16 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateIvUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    switch(config('database.default'))
     {
+      case('pgsql'):
         Schema::create('iv_users', function (Blueprint $table) {
             $table->increments('id');
             $table->text('email');
@@ -23,15 +26,29 @@ class CreateIvUsersTable extends Migration
             $table->timestampTz('updated_at')->useCurrent();
             $table->unique(['email','username']);
         });
+        break;
+      case('mysql'):
+        Schema::create('iv_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('username');
+            $table->boolean('confirmed');
+            $table->rememberToken();
+            $table->timestampTz('created_at')->useCurrent();
+            $table->timestampTz('updated_at')->useCurrent();
+            $table->unique(['email','username']);
+        });
+        break;
     }
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('iv_users');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('iv_users');
+  }
 }
