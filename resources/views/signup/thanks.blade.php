8608cfb8b4ce32028aeef49837e114bf44e9fc0e
amoschou <github@amoschou.net>
2018-02-11 13:32:25 +1030

    Update thanks.blade.php

--- resources/views/signup/thanks.blade.php
+++ resources/views/signup/thanks.blade.php
@@ -3,5 +3,6 @@
 @section('content')
 <div class="container">
   <p>Thank you, please check your email for further instructions.</p>
+  <p>You will need to open the access link in <strong>this</strong> browser.</p>
 </div>
 @endsection
