925f3bf0d67a7cccf8a2f703b9d0ca1b0279c347
Andrew Gilbert Moschou <amoschou@fastmail.com.au>
2018-03-01 02:50:26 +1030

    POST tests

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,31 +10,17 @@ class PostController extends Controller
 {
   public function post(Request $request)
   {
-    $requestjson = json_encode($request->all(),JSON_UNESCAPED_SLASHES);
-    Log::info('Received post (A): ' . $requestjson);
+    $requestjson = $request->json;
     DB::table('html_posts')->insert([
       'postjson' => $requestjson,
     ]);
-    // Ideally, this should send some HTTP response (e.g. 200) back to Stripe
-
-
-
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-\Stripe\Stripe::setApiKey(config('services.stripe.secret'));
-
-// Retrieve the request's body and parse it as JSON
-$input = @file_get_contents("php://input");
-$event_json = json_decode($input);
-
-// Do something with $event_json
-    Log::info('Received post (B): ' . $input);
-    Log::info('Reeived post (C): ' . var_export($event_json,TRUE));
+    // If this failed, then exception happens.
+    // Otherwise all good and return 200.
+    return response()->setStatusCode(200);
   }
 
   public function get()
   {
-    
     return view('post.post');
   }
 }
