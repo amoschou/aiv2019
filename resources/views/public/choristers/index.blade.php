31dc35e2a9bafa3de9a8c2b69b335dc4352019c6
Andrew Gilbert Moschou <amoschou@fastmail.com.au>
2018-08-26 22:05:20 +0930

    Cities leader board

--- resources/views/public/choristers/index.blade.php
+++ resources/views/public/choristers/index.blade.php
@@ -2,31 +2,65 @@
 
 @section('extracontent')
 <h3>Who’s coming</h3>
+<p>So far, we have choristers joining the festival from the following cities and choirs.</p>
+@php
+  $q = "WITH a AS (
+          SELECT *
+          FROM f_aicsachoirs
+          UNION
+          SELECT *
+          FROM f_nonaicsachoirs
+        ), b AS (
+          SELECT userid,1/count(choirshortname) AS scaled
+          FROM a
+          GROUP BY userid
+        ), c AS (
+          SELECT city,round(SUM(scaled)) as numfrom
+          FROM a NATURAL JOIN b
+          GROUP BY city
+        )
+        SELECT * FROM c";
+  $citycount = DB::select($q,[]);
+@endphp
+  <table>
+    <thead>
+      <tr><th>City</th><th>Choristers</th></tr>
+    </thead>
+    <tbody>
+      @foreach($citycount as $cityrow)
+        <tr>
+          <th>{{ $cityrow->city }}</th><td>{{ $cityrow->numfrom }}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
 @php
   $q = "SELECT
-  choirname,
-  choirshortname,
-  count(userid) as count
-FROM (
-  SELECT * FROM f_aicsachoirs
-  UNION SELECT * FROM f_nonaicsachoirs
-) T
-GROUP BY
-  choirname,
-  choirshortname
-ORDER BY
-  count DESC";
+          choirname,
+          choirshortname,
+          choirprintname,
+          count(userid) as count
+        FROM (
+          SELECT userid,choirname,choirshortname,concat(choirshortname,' (',choirname,')') as choirprintname FROM f_aicsachoirs
+          UNION SELECT userid,choirname,choirshortname,choirname as choirprintname FROM f_nonaicsachoirs
+        ) T
+        GROUP BY
+          choirname,
+          choirshortname,
+          choirprintname
+        ORDER BY
+          count DESC";
 
   $choircount = DB::select($q,[]);
 @endphp
   <table>
     <thead>
-      <tr><th>Choir</th><th>Choirsters</th></tr>
+      <tr><th>Choir</th><th>Choristers</th></tr>
     </thead>
     <tbody>
       @foreach($choircount as $choirrow)
         <tr>
-          <th>{{ $choirrow->choirname }}</th><td>{{ $choirrow->count }}</td>
+          <th>{{ $choirrow->choirprintname }}</th><td>{{ $choirrow->count }}</td>
         </tr>
       @endforeach
     </tbody>
