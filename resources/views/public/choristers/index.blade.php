7caf36dc4ed2368cf0755211933ea9ea94facb38
Andrew Gilbert Moschou <amoschou@fastmail.com.au>
2018-08-26 22:44:04 +0930

    1

--- resources/views/public/choristers/index.blade.php
+++ resources/views/public/choristers/index.blade.php
@@ -4,7 +4,37 @@
 <h3>Who’s coming</h3>
 <p>So far, we have choristers joining the festival from the following cities and choirs.</p>
 @php
-
+  $q = "WITH a AS (
+          SELECT *
+          FROM f_aicsachoirs
+          UNION
+          SELECT *
+          FROM f_nonaicsachoirs
+        ), b AS (
+          SELECT userid,1/count(choirshortname) AS scaled
+          FROM a
+          GROUP BY userid
+        ), c AS (
+          SELECT city,ROUND(SUM(scaled)) as numfrom
+          FROM a NATURAL JOIN b
+          GROUP BY city
+        )
+        SELECT * FROM c";
+  $citycount = DB::select($q,[]);
+@endphp
+  <table>
+    <thead>
+      <tr><th>Choir</th><th>Choristers</th></tr>
+    </thead>
+    <tbody>
+      @foreach($citycount as $cityrow)
+        <tr>
+          <th>{{ $cityrow->city }}</th><td>{{ $cityrow->numfrom }}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+@php
   $q = "SELECT
           choirprintname,
           count(userid) as count
@@ -19,7 +49,6 @@
           choirprintname
         ORDER BY
           count DESC";
-
   $choircount = DB::select($q,[]);
 @endphp
   <table>
