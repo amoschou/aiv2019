1f4c3b45c491c0680b89e1bf5126529ed8031c92
Andrew Gilbert Moschou <amoschou@fastmail.com.au>
2018-08-26 22:15:17 +0930

    Revert

--- resources/views/public/choristers/index.blade.php
+++ resources/views/public/choristers/index.blade.php
@@ -4,37 +4,6 @@
 <h3>Who’s coming</h3>
 <p>So far, we have choristers joining the festival from the following cities and choirs.</p>
 @php
-  $q = "WITH a AS (
-          SELECT *
-          FROM f_aicsachoirs
-          UNION
-          SELECT *
-          FROM f_nonaicsachoirs
-        ), b AS (
-          SELECT userid,1/count(choirshortname) AS scaled
-          FROM a
-          GROUP BY userid
-        ), c AS (
-          SELECT city,round(SUM(scaled)) as numfrom
-          FROM a NATURAL JOIN b
-          GROUP BY city
-        )
-        SELECT * FROM c";
-  $citycount = DB::select($q,[]);
-@endphp
-  <table>
-    <thead>
-      <tr><th>City</th><th>Choristers</th></tr>
-    </thead>
-    <tbody>
-      @foreach($citycount as $cityrow)
-        <tr>
-          <th>{{ $cityrow->city }}</th><td>{{ $cityrow->numfrom }}</td>
-        </tr>
-      @endforeach
-    </tbody>
-  </table>
-@php
   $q = "SELECT
           choirname,
           choirshortname,
