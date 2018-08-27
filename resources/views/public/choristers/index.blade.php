935841267b80102e3c2ca41f46fdfb857a73759e
Andrew Gilbert Moschou <amoschou@fastmail.com.au>
2018-08-28 00:26:22 +0930

    Choir long names only, the bug won't let short names happen

--- resources/views/public/choristers/index.blade.php
+++ resources/views/public/choristers/index.blade.php
@@ -31,9 +31,9 @@
           choirprintname,
           count(userid) as count
         FROM (
-          SELECT userid,choirname,choirshortname,CONCAT(choirshortname,' (',choirname,')') as choirprintname FROM f_aicsachoirs
+          SELECT userid,choirname,choirshortname,choirname as choirprintname FROM f_aicsachoirs
           UNION
-          SELECT userid,choirname,choirshortname,CONCAT('',choirname) as choirprintname FROM f_nonaicsachoirs
+          SELECT userid,choirname,choirshortname,choirname as choirprintname FROM f_nonaicsachoirs
         ) T
         GROUP BY
           choirname,
