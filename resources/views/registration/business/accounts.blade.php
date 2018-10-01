0d62ed427f89594ecd162f85eb4ab6d939bed24f
Andrew Gilbert Moschou <amoschou@fastmail.com.au>
2018-10-02 08:43:10 +0930

    Receipts accounts

--- resources/views/registration/business/accounts.blade.php
+++ resources/views/registration/business/accounts.blade.php
@@ -4,7 +4,18 @@
   <h1>AIVCF Adelaide<br><small><span class="font-weight-bold">ABN</span> 41 628 114 920</small></h1>
   <p class="text-right lead">Date: {{ date('l, j F Y') }}</p>
   <p class="font-weight-bold">No GST has been charged.</p>
+  @php
+    \Stripe\Stripe::setApiKey(config('services.stripe.secret'));
+      function centstodollarsandcents($a) {
+        $hundredths = $a % 10;
+        $a = (int) ($a - $hundredths)/10;
+        $tenths = $a % 10;
+        $a = (int) ($a - $tenths)/10;
+        return '$' . $a . '.' . $tenths . $hundredths ;
+      }
+  @endphp
   @foreach($people as $person)
+    <hr>
     @php
       $accountref = DB::table('iv_users')->select('accountref')->where('id',$person->id)->first()->accountref;
     @endphp
@@ -50,89 +61,69 @@
         </tr>
       </tfoot>
     </table>
-  @endforeach
-  {{--
-  <h2>Receipts</h2>
-  <h3>Card payments</h3>
-  @php
-    \Stripe\Stripe::setApiKey(config('services.stripe.secret'));
-    $charges = DB::table('rego_stripe_charges')->select('chargeid')->where('accountref',$accountref)->get();
-    function centstodollarsandcents($a) {
-      $hundredths = $a % 10;
-      $a = (int) ($a - $hundredths)/10;
-      $tenths = $a % 10;
-      $a = (int) ($a - $tenths)/10;
-      return '$' . $a . '.' . $tenths . $hundredths ;
-    }
-  @endphp
-  <table class="table table-sm">
-    <thead>
-      <tr>
-        <th class="pl-0">Charge ID</th>
-        <th>Date</th>
-        <th>Status</th>
-        <th class="text-right">Transaction amount</th>
-        <th class="text-right pr-0">Transaction net</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($charges as $charge)
-        @php
-          $chargeobject = \Stripe\Charge::retrieve($charge->chargeid);
-          $balancetransactionobject = \Stripe\BalanceTransaction::retrieve($chargeobject->balance_transaction);
-        @endphp
+    <h2>Receipts</h2>
+    <h3>Card payments</h3>
+    @php
+      $charges = DB::table('rego_stripe_charges')->select('chargeid')->where('accountref',$accountref)->get();
+    @endphp
+    <table class="table table-sm">
+      <thead>
         <tr>
-          <td class="pl-0">{{ $chargeobject->id }}</td>
-          <td>{{ date('j/m/y',$chargeobject->created) }}</td>
-          <td>
-            {{ $chargeobject->status }}
-            @if( $chargeobject->status === 'failed' )
-              ({{ $chargeobject->failure_message }})
-            @endif
-          </td>
-          @if( $chargeobject->captured )
-            <td class="text-right">{{ centstodollarsandcents($balancetransactionobject->amount) }}</td>
-            <td class="text-right pr-0">{{ centstodollarsandcents($balancetransactionobject->net) }}</td>
-          @else
-            <td class="text-right"></td>
-            <td class="text-right pr-0"></td>
-          @endif
+          <th class="pl-0">Charge ID</th>
+          <th>Date</th>
+          <th>Status</th>
+          <th class="text-right">Transaction amount</th>
+          <th class="text-right pr-0">Transaction net</th>
         </tr>
-      @endforeach
-    </tbody>
-  </table>
-  <h3>Electronic bank transfer</h3>
-  <table class="table table-sm">
-    <thead>
-      <tr>
-        <th class="pl-0">Transaction ID</th>
-        <th>Date</th>
-        <th>Description</th>
-        <th class="text-right pr-0">Credit</th>
-      </tr>
-    </thead>
-    <tbody>
-      @php
-        $bankq = "SELECT id,date,description,credit FROM bank_transaction_accounts JOIN bank_transactions ON (id = transactionid) WHERE accountref = ?";
-        $transactions = DB::SELECT($bankq,[$accountref]);
-      @endphp
-      @foreach($transactions as $transaction)
-        <td clas="pl-0">{{ $transaction->id }}</td>
-        <td class="">{{ $transaction->date }}</td>
-        <td class="">{{ $transaction->description }}</td>
-        <td class="text-right pr-0">${{ $transaction->credit }}</td>
-      @endforeach
-    </tbody>
-  </table>
-  <h3>How to pay</h3>
-  <h4>Preferred payment option</h4>
-  <p>Payments can be made by debit/credit card (Including international cards) at <a href="/payments/checkout?ref={{ $accountref }}">https://www.aiv.org.au/payments/checkout?ref={{ $accountref }}</a>.</p>
-  <p>When you pay with card and include your account reference number, your payment will automatically and immediately be reflected on your invoice.</p>
-  <h4>Other payment option</h4>
-  <p>Or payments can be made by bank transfer to the account BSB&nbsp;105-120, Account number&nbsp;027885840, Account name&nbsp;<em>AIVCF Adelaide</em>.</p>
-  <p>When you pay by bank transfer, there will be a manual processing delay and your invoice won’t be reflected immediately.</p>
-  <h4>Fees and due dates</h4>
-  <p>Fees are available from <a href="/participate/choir">https://www.aiv.org.au/participate/choir</a> and the cost of any merchandise sales or music sales are additional.</p>
-  <p>Complete payment by the published timeline (on page 2 of <a href="/documents/newsbulletins/adelaideiv2019news4.pdf">News bulletin 4</a>)</p>
-  --}}
+      </thead>
+      <tbody>
+        @foreach($charges as $charge)
+          @php
+            $chargeobject = \Stripe\Charge::retrieve($charge->chargeid);
+            $balancetransactionobject = \Stripe\BalanceTransaction::retrieve($chargeobject->balance_transaction);
+          @endphp
+          <tr>
+            <td class="pl-0">{{ $chargeobject->id }}</td>
+            <td>{{ date('j/m/y',$chargeobject->created) }}</td>
+            <td>
+              {{ $chargeobject->status }}
+              @if( $chargeobject->status === 'failed' )
+                ({{ $chargeobject->failure_message }})
+              @endif
+            </td>
+            @if( $chargeobject->captured )
+              <td class="text-right">{{ centstodollarsandcents($balancetransactionobject->amount) }}</td>
+              <td class="text-right pr-0">{{ centstodollarsandcents($balancetransactionobject->net) }}</td>
+            @else
+              <td class="text-right"></td>
+              <td class="text-right pr-0"></td>
+            @endif
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+    <h3>Electronic bank transfer</h3>
+    <table class="table table-sm">
+      <thead>
+        <tr>
+          <th class="pl-0">Transaction ID</th>
+          <th>Date</th>
+          <th>Description</th>
+          <th class="text-right pr-0">Credit</th>
+        </tr>
+      </thead>
+      <tbody>
+        @php
+          $bankq = "SELECT id,date,description,credit FROM bank_transaction_accounts JOIN bank_transactions ON (id = transactionid) WHERE accountref = ?";
+          $transactions = DB::SELECT($bankq,[$accountref]);
+        @endphp
+        @foreach($transactions as $transaction)
+          <td clas="pl-0">{{ $transaction->id }}</td>
+          <td class="">{{ $transaction->date }}</td>
+          <td class="">{{ $transaction->description }}</td>
+          <td class="text-right pr-0">${{ $transaction->credit }}</td>
+        @endforeach
+      </tbody>
+    </table>
+  @endforeach
 @endsection
