0f8040e6427686dd3d2972a9dd92302465e8a207
Andrew Gilbert Moschou <amoschou@fastmail.com.au>
2019-01-02 04:58:34 +1030

    Meet and greet

--- resources/views/registration/meetandgreet.blade.php
+++ resources/views/registration/meetandgreet.blade.php
@@ -560,7 +560,7 @@
                 $notes = DB::table('rego_notes')->select('notes')->where('userid',$person->id)->get();
               @endphp
               @foreach($notes as $note)
-                <p>{{ $note->notes) }}</p>
+                <p>{{ var_dump($note) }}</p>
               @endforeach()
             </div>
           </div>
