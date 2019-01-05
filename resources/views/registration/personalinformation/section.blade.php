f4fcab2bddd9abbb938952113a07620de1305be4
Andrew Gilbert Moschou <amoschou@fastmail.com.au>
2019-01-06 00:05:34 +1030

    Data tables

--- resources/views/registration/personalinformation/section.blade.php
+++ resources/views/registration/personalinformation/section.blade.php
@@ -58,7 +58,7 @@
       @break
   @endswitch
   
-  <table id="datatable" class="table table-sm table-striped table-bordered">
+  <table id="datatable2" class="table table-sm table-striped table-bordered">
     <thead class="thead-dark">
       @foreach($h as $hh)
         <th scope="col">{{ $hh }}</th>
@@ -78,6 +78,7 @@
     
 @endsection
 
+{{--
 @section('extrastyles')
   <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
   @endsection
@@ -91,5 +92,31 @@
     } );
   </script>
 @endsection
+--}}
 
+
+@section('extrastyles')
+  <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+  <link href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css" rel="stylesheet">
+  @endsection
+@section('extrascripts')
+  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+  <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.flash.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+  <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+  <script>
+    $(document).ready(function() {
+        $('#datatable2').DataTable({
+            dom: 'Bfrtip',
+            buttons: ['copy','csv','excel','pdf','print']
+        });
+    });
+  </script>
+@endsection
 
